500044c9ddb0003f13a5c4b3e9c2dca0fd38a33f
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-16 22:42:33 -0500

    feat: added Directory(data), and File(contacts).

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        //We will be building a command line application for keeping track of "contacts".
-        // A contact will be comprised of a a name and phone number combination.
-        //
-        //The application should be able to:
-        //
-        //Show all your contacts
-        //Add a new contact
-        //Search a contact by her name
-        //Delete an existing contact
 
         String directory = "data";
         String filename = "contacts.txt";
@@ -22,11 +14,40 @@ public class Main {
 
         if(Files.notExists(dataDirectory)) {
             try {
-                Files.creatDirectories(dataDirectory)
+                Files.createDirectory(dataDirectory);
+            } catch (IOException errorHandler) {
+                System.out.println("Problems occurred when creating new DIRECTORY.");
+                errorHandler.printStackTrace();
             }
         }
 
+        if(Files.notExists(dataFile)) {
+            try {
+                Files.createDirectory(dataFile);
+            } catch (IOException errorHandler) {
+                System.out.println("Problems occurred when creating new FILE.");
+                errorHandler.printStackTrace();
+            }
+        }
+
+        System.out.println("If succeed!");
+        System.out.println("This will create both Directory: data, and contacts inside of Directory \"in theory\".");
 
     }
 }
 
+/*
+The user interface for your application should include a main menu like the following,
+where the user will need to enter a number between 1 and 5:
+1. View contacts.
+2. Add a new contact.
+3. Search a contact by name.
+4. Delete an existing contact.
+5. Exit.
+Enter an option (1, 2, 3, 4 or 5):
+
+// todo method call APPLICATION
+// todo method that returns a List of Contact objects.
+//
+
+ */
