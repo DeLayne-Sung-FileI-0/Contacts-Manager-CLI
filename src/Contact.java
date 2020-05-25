18d18696667e4029994fc826f7fec945fb4408c2
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-24 23:46:46 -0500

    redo the pair programming, after seeing the Fernando, and Douglas doing it.

---
+++ src/Contact.java
@@ -0,0 +1,43 @@
+
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Contact(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+}
+
+// todo Note: In ths Contact class, we will store both firstName, lastName, and phoneNumber.
+//  To get the information inside of class and outside of class, you need getters and setters for fn,ln, and pn.
---
+++ src/ContactApp.java
@@ -0,0 +1,64 @@
+import java.io.File;
+
+public class ContactApp {
+    public static void main(String[] args) {
+//        FileIO.createDirs();
+//
+//        // CREATING New contacts
+//        Contact sung = new Contact("Sung Il", "Lee", "8178799506");
+//        Contact delayne = new Contact("DeLayne", "LaBove", "8179408605");
+//        Contact jenny = new Contact("Jennifer", "Lee", "2545930952");
+//        Contact jacques = new Contact("Jacques", "Boutte", "8178765342");
+//
+//        FileIO.writeContactToFile(sung);
+//        FileIO.writeContactToFile(delayne);
+//        FileIO.writeContactToFile(jenny);
+//        FileIO.writeContactToFile(jacques);
+//
+//        FileIO.readFile();
+//        FileIO.search("Sung");
+//        FileIO.search("Jacques");
+
+//        FileIO.delete("2545930952");
+//        FileIO.delete("8179408605");
+
+
+        Input input = new Input();
+
+        while(true) {
+            System.out.println("1. View contacts.\n" +
+                    "2. Add a new contact.\n" +
+                    "3. Search a contact by name.\n" +
+                    "4. Delete an existing contact.\n" +
+                    "5. Exit.\n" +
+                    "Enter an option (1, 2, 3, 4 or 5):");
+            int option = input.getInt(1,5);
+            switch (option) {
+                case 1:
+                    FileIO.readFile();
+                    break;
+                case 2:
+                    String firstName = input.getString("Give me the First Name");
+                    String lastName = input.getString("Give me the Last Name");
+                    String phoneNum = input.getString("Give me the Phone Number");
+                    Contact contact = new Contact(firstName, lastName, phoneNum);
+                    FileIO.writeContactToFile(contact);
+                    break;
+                case 3:
+                    String term = input.getString("Give me the name to search");
+                    FileIO.search(term);
+                    break;
+                case 4:
+                    String aNumber = input.getString("Give me the phone number to delete");
+                    FileIO.delete(aNumber);
+                    break;
+                case 5:
+                    System.exit(0);
+
+            }
+        }
+
+    }
+}
+
+// todo Note: This class will have main methods for running methods
